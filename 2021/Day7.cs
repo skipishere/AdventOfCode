61ebc9a02e987eb8f53f09b201d1801b2c8777fd
Stephen Jones <stephen.jones@red-gate.com>
2021-12-07 09:49:39 +0000

    Day 7 part 1 and 2

---
+++ 2021/Day7.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCode2021
+{
+    internal class Day7 : Day
+    {
+
+        public override string Name => "day7";
+
+        private IEnumerable<int> _crabPosition = new List<int>();
+
+        public Day7()
+        {
+            _crabPosition = this.InputString().First().Split(',').Select(c => int.Parse(c));
+        }
+
+        public int CalculateFuel(bool triangular)
+        {
+            var answer = new Dictionary<int, int>();
+
+            for (int i = 1; i < _crabPosition.Max(); i++)
+            {
+                var fuelSpent = _crabPosition.Sum(c =>
+                {
+                    var movement = 0;
+                    movement = (c > i) ? c - i : i - c;
+                    
+                    if (triangular)
+                    {
+                        return movement * (movement + 1) / 2;
+                    }
+
+                    return movement;
+                });
+
+                answer.Add(i, fuelSpent);
+            }
+
+            return answer.Values.Min();
+        }
+
+        public override void FirstAnswer()
+        {
+            Console.WriteLine(CalculateFuel(false));
+        }
+
+
+        public override void SecondAnswer()
+        {
+            Console.WriteLine(CalculateFuel(true));
+        }
+    }
+}
