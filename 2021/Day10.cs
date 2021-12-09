20634aabceebdc3e6ebed4bd1eec3c68711789e6
Stephen Jones <stephen.jones@red-gate.com>
2021-12-09 23:51:36 +0000

    Prep for Day 10

---
+++ 2021/Day10.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode2021
+{
+    internal class Day10 : Day
+    {
+        public override string Name => "Day 10: ";
+
+        
+
+        public Day10()
+        {
+            var input = this.InputString();
+
+        
+        }
+
+        public override void FirstAnswer()
+        {
+        
+            Console.WriteLine();
+        }
+
+
+        public override void SecondAnswer()
+        {
+            
+            Console.WriteLine();
+        }
+    }
+}
