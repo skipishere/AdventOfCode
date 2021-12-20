2743828ee4325be1320211d3b70e4d58b5c6adb4
Stephen Jones <stephen.jones@red-gate.com>
2021-12-20 09:45:45 +0000

    Day 17 creation

---
+++ 2021/Day17.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2021
+{
+    internal class Day17 : Day
+    {
+        public override string Name => "Day 17: Trick Shot";
+                
+        public override void FirstAnswer()
+        {
+            Console.WriteLine(1);
+        }
+
+        public override void SecondAnswer()
+        {
+            Console.WriteLine(2);
+        }
+    }
+}
