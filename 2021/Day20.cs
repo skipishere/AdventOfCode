582befbb367d7607e3b07bec28bc8855f1ed6f0b
Stephen Jones <stephen.jones@red-gate.com>
2021-12-20 09:45:55 +0000

    Day 20 creation

---
+++ 2021/Day20.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2021
+{
+    internal class Day20 : Day
+    {
+        public override string Name => "Day 20: Trench Map";
+                
+        public override void FirstAnswer()
+        {
+            Console.WriteLine(1);
+        }
+
+        public override void SecondAnswer()
+        {
+            Console.WriteLine(2);
+        }
+    }
+}
