7a3652d5ba2450cb9394131c4d441267491f627a
Stephen.Jones <stephen.jones@red-gate.com>
2022-12-01 08:43:47 +0000

    Day 1 part 1 & 2 solution

--- 2022/Day01.cs
+++ 2022/Day01.cs
@@ -2,16 +2,56 @@
 {
     internal record Day01 : Day
     {
-        public override string Name => "Day 1: ";
+        public override string Name => "Day 1: Calorie Counting";
+
+        private List<Elf> elves = new();
+
+        public Day01()
+        {
+            var currentElf = new Elf();
+            
+            foreach (var calorie in InputString())
+            {
+                if (calorie == string.Empty)
+                {
+                    elves.Add(currentElf);
+                    currentElf = new Elf();
+                }
+                else
+                {
+                    currentElf.Add(calorie);
+                }
+            }
+
+            elves.Add(currentElf);
+        }
 
         public override void FirstAnswer()
         {
-            Console.WriteLine("1.1");
+            Console.WriteLine(elves.Max(c => c.TotalCalories));
         }
 
         public override void SecondAnswer()
         {
-            Console.WriteLine("1.2");
+            var total = elves
+                .OrderByDescending(c => c.TotalCalories)
+                .Take(3)
+                .Sum(c => c.TotalCalories);
+
+            Console.WriteLine(total);
+        }
+
+        private record Elf
+        {
+            public List<int> Calories = new();
+
+            public int TotalCalories => this.Calories.Sum();
+
+            public void Add(string calorie)
+            {
+                Calories.Add(int.Parse(calorie));
+            }
         }
     }
+
 }
