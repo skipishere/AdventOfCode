5afad26a2cea0f732f362e9ec0df2eef1d12e426
Stephen.Jones <stephen.jones@red-gate.com>
2022-12-04 09:17:15 +0000

    Day 4 maths refactoring

--- 2022/Day04.cs
+++ 2022/Day04.cs
@@ -4,7 +4,7 @@ internal record Day04 : Day
 {
     public override string Name => "Day 2: Camp Cleanup";
 
-    private new List<Tuple<Job, Job>> _pairs = new();
+    private readonly List<Tuple<Job, Job>> _pairs = new();
 
     public Day04()
     {
@@ -17,6 +17,7 @@ internal record Day04 : Day
 
     public override object FirstAnswer()
     {
+        //_pairs.Count(c => c.Item1.Within(c.Item2));
         var sets =
             _pairs.Count(c =>
                 c.Item1.Start >= c.Item2.Start && c.Item1.Finish <= c.Item2.Finish
@@ -26,27 +27,33 @@ internal record Day04 : Day
     }
 
     public override object SecondAnswer()
-    {
-        var sets =
-            _pairs.Count(c =>
-                (c.Item1.Start >= c.Item2.Start && c.Item1.Start <= c.Item2.Finish)
-                || (c.Item1.Finish >= c.Item2.Start && c.Item1.Finish <= c.Item2.Finish)
-                || (c.Item2.Start >= c.Item1.Start && c.Item2.Start <= c.Item1.Finish)
-                || (c.Item2.Finish >= c.Item1.Start && c.Item2.Finish <= c.Item1.Finish));
-
-        return sets;
+    {        
+        return _pairs.Count(c => c.Item1.Intersec(c.Item2));
     }
 
     private record Job
     {
         public int Start { get; set; }
         public int Finish { get; set; }
+        public int Length { get; private set; }
 
         public Job(string input)
         {
             var split = input.Split("-");
             Start = int.Parse(split[0]);
             Finish = int.Parse(split[1]);
+            Length = Finish - Start;
+        }
+
+        public bool Within(Job otherJob)
+        {
+            return false;
+        }
+
+        public bool Intersec(Job otherJob)
+        {
+            return
+                Math.Max(otherJob.Start, this.Start) <= Math.Min(otherJob.Finish, this.Finish);
         }
     }
 }
